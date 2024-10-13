fb37212bdb124c16ed71fd7055ef2500ceb83a46
Mussorable <redduck5601@gmail.com>
2024-10-13 09:20:41 +0200

    added compose.yaml for all projects

--- consumer.py
+++ consumer.py
@@ -15,11 +15,12 @@ class ConsumerThread(threading.Thread):
 
         self.app = app
         self.consumer = Consumer({
-            'bootstrap.servers': 'localhost:9092',
+            'bootstrap.servers': 'kafka:9092',
             'group.id': 'login_group',
             'auto.offset.reset': 'earliest',
         })
         self.consumer.subscribe([topic])
+        logger.info(f'Subscribed to topic: {topic}')
         self._running = True
 
     def run(self):
@@ -30,13 +31,20 @@ class ConsumerThread(threading.Thread):
             if msg is None:
                 logger.info("No messages received")
                 continue
-            if msg.error:
-                logger.error(f'Consumer error: {msg.error}')
-                continue
+            # if msg.error():
+            #     logger.error(f'Consumer error: {msg.error().str()}')
+            #     continue
+            else:
+                logger.info(f'Received message: {msg.value().decode("utf-8")}')
 
-            data_info = json.loads(msg.value.decode('utf-8'))
-            with self.app.app_context():
-                self.process_message(data_info)
+            try:
+                logger.info(f'Message value: {msg.value()}')
+                data_info = json.loads(msg.value().decode('utf-8'))
+                logger.info(f'Parsed message: {data_info}')
+                with self.app.app_context():
+                    self.process_message(data_info)
+            except Exception as e:
+                logger.error(f"Error processing message: {e}")
 
         logger.info("Stopping consumer thread...")
         self.consumer.close()
@@ -55,3 +63,4 @@ class ConsumerThread(threading.Thread):
 
     def stop(self):
         self._running = False
+        logger.info("Stopping consumer thread...")
--- manager.py
+++ manager.py
@@ -1,7 +1,24 @@
-from app import create_app
+import logging
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+from app import create_app, db
 from consumer import ConsumerThread
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('kafka')
 
 app = create_app()
 
 consumer = ConsumerThread('user-tokens', app)
 consumer.start()
+
+if not consumer.is_alive():
+    logger.info('Consumer thread is not running')
+else:
+    logger.info('Consumer thread is running')
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'app': app, 'db': db, 'sa': sa, 'so': so, 'consumer': consumer}
